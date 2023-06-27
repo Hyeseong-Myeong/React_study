da52c2f8ed56ab4583c97638c54fe30855f66402
Hyeseong-Myeong <a01025819869@gmail.com>
2023-06-27 12:32:43 +0900

    Todo setup

--- react_prtc/src/App.js
+++ react_prtc/src/App.js
@@ -1,28 +1,10 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 
-function Hello()  {
-
-  useEffect(() => {
-    console.log("hi")
-    return ()=> console.log("bi")
-  }, [])
-
-  return <h1>Hello</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-
-  const onShowingClick = () => setShowing((prev) => !prev);
-
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onShowingClick}>{showing ? "Hide" : "show"}</button>
-
+      
     </div>
   );
 }
