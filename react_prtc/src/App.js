19e67f5af6b0a58ade8d915a14f7cf2f1a3d0024
Hyeseong-Myeong <a01025819869@gmail.com>
2023-06-27 09:40:11 +0900

    clean up function

--- react_prtc/src/App.js
+++ react_prtc/src/App.js
@@ -3,41 +3,24 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setkeyword] = useState("");
-  const [showing, setShowing] = useState(false);
+function Hello()  {
 
-  const onClick = () => setValue((prev) => prev + 1);
-  
-  const onChange = (event) => setkeyword(event.target.value);
+  useEffect(() => {
+    console.log("hi")
+    return ()=> console.log("bi")
+  }, [])
+
+  return <h1>Hello</h1>
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
 
   const onShowingClick = () => setShowing((prev) => !prev);
 
-  console.log("I run all the time")
-
-  const RunOnlyOnce = () => {
-    console.log("I run only once")
-  }
-  useEffect(RunOnlyOnce, [])
-
-  useEffect(() => {
-    if(keyword !== "" && keyword.length > 3){
-      console.log("Search for ", keyword);
-    }
-  }, [keyword])
-
   return (
     <div>
-      <input 
-        type="text"
-        placeholder="Search hear..." 
-        value={keyword}
-        onChange={onChange}
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-      <Button  text={"Button"}/>
+      {showing ? <Hello /> : null}
       <button onClick={onShowingClick}>{showing ? "Hide" : "show"}</button>
 
     </div>
