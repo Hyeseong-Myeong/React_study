43d64bd91e41611ef1e377709309177914752242
Hyeseong-Myeong <a01025819869@gmail.com>
2023-06-27 13:07:53 +0900

    todo list

--- react_prtc/src/App.js
+++ react_prtc/src/App.js
@@ -2,11 +2,38 @@ import { useState, useEffect } from "react";
 
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+	const [todo, setToDo]  = useState("");
+	const [todos, setTodos] = useState([]);
+
+	const onChange = (event) => setToDo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if(todo === ""){
+			return;
+		}
+		setTodos(currentArray => [todo, ...currentArray]);
+		setToDo("");
+	}
+	
+
+	return (
+		<div>
+			<h1>My To Dos ({todos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					onChange={onChange} 
+					value={todo} 
+					type="text" 
+					placeholder="Write your ToDo..." 
+				/>
+				<button>Add To Do</button>
+			</form>
+			<hr/>
+			<ul>
+				{todos.map((item, index) => <li key={index}>{item}</li>)}	
+			</ul>
+		</div>
+	);
 }
 
 export default App;
